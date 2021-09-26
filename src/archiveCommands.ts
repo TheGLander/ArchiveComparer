a4d86d2e8b76be621099b3712c2fbefe778af40d
Lubomir <lubomir.yudenko@gmail.com>
2021-09-26 11:54:13 +0300

    Add 7z

--- src/archiveCommands.ts
+++ src/archiveCommands.ts
@@ -8,6 +8,7 @@ interface ArchiveCompetitor {
 const archiveCommands: Record<string, ArchiveCompetitor> = {
 	zip: { add: "zip -r $o $i", extract: "unzip $i -d $o" },
 	rar: { add: "rar a $o $i", extract: "rar x $i $o/" },
+	"7z": { add: "7z a $o $i", extract: "7z x $i $o/" },
 }
 
 archiveCommands["tar.gz"] =
